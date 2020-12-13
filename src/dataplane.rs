187a8f3e847d4e0791f83355e5ccec0431d5d3ce
Ravi Vantipalli (Intel) <vantipal@vantipal-mac02.local>
2020-12-13 13:42:33 -0800

    dataplane changes

--- src/dataplane.rs
+++ src/dataplane.rs
@@ -1,36 +1,25 @@
 extern crate pnet;
 
 use crate::packet::Packet;
+use pnet::datalink::dummy;
 use pnet::datalink::Channel::Ethernet;
 use pnet::datalink::{self, NetworkInterface};
 use pnet::datalink::{DataLinkReceiver, DataLinkSender};
-use pnet::datalink::dummy;
+
+use std::sync::mpsc;
 
 fn create_conn(intf: &str) -> (Box<dyn DataLinkSender>, Box<dyn DataLinkReceiver>) {
     let interface_names_match = |iface: &NetworkInterface| iface.name == intf;
     let interfaces = datalink::interfaces();
     println!("{:?}", interfaces);
-    let interface = interfaces.clone()
+    let interface = interfaces
+        .clone()
         .into_iter()
         .filter(interface_names_match)
         .next()
         .unwrap();
     // Create a new channel, dealing with layer 2 packets
     let (mut tx, mut rx) = match datalink::channel(&interface, Default::default()) {
-        Ok(Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("Unhandled channel type"),
-        Err(e) => panic!(
-            "An error occurred when creating the datalink channel: {}",
-            e
-        ),
-    };
-    (tx, rx)
-}
-
-fn create_dummy_conn(intf: u8) -> (Box<dyn DataLinkSender>, Box<dyn DataLinkReceiver>) {
-    let interface = dummy::dummy_interface(intf);
-    // Create a new channel, dealing with layer 2 packets
-    let (mut tx, mut rx) = match dummy::channel(&interface, dummy::Config::default()) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
         Ok(_) => panic!("Unhandled channel type"),
         Err(e) => panic!(
@@ -59,6 +48,13 @@ fn verify_packet(rx: &mut Box<dyn DataLinkReceiver + 'static>, pkt: &Packet) {
         }
     }
 }
+
+fn create_mpsc_conn(intf: u8) -> (mpsc::Sender<Packet>, mpsc::Receiver<Packet>) {
+    // Create a new channel, dealing with layer 2 packets
+    let (mut tx, mut rx) = mpsc::channel();
+    (tx, rx)
+}
+
 #[test]
 fn test_send_packet() {
     let pkt = crate::create_tcp_packet(
@@ -87,7 +83,8 @@ fn test_send_packet() {
         false,
         100,
     );
-    let (mut tx, mut rx) = create_dummy_conn(0);
+
+    let (mut tx, mut rx) = create_conn("lo0");
     // let (_, mut rx) = create_dummy_conn(1);
     send_packet(&mut tx, &pkt);
     verify_packet(&mut rx, &pkt);
