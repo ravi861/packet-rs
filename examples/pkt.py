cf3d8ecde67c251b7d17db54b8f55e58cc4439c7
Ravi Vantipalli (Intel) <vantipal@vantipal-mac02.local>
2021-01-03 20:25:37 -0800

    update examples

--- examples/pkt.py
+++ examples/pkt.py
@@ -1,43 +1,42 @@
 from rscapy import *
 
-x = Ethernet()
-x.show()
-etype = x.etype
+# create an ethernet header
+eth = Ethernet()
+eth.show()
+
+# get ethertype using idiomatic python get
+etype = eth.etype
 print(etype)
-x.etype = 0x8100
-x.show()
 
-v = Vxlan()
-v.show()
+# set ethertype using idiomatic python set
+eth.etype = 0x8100
+eth.show()
 
-p = Packet.create_tcp_packet("00:11:11:11:11:11", "00:06:07:08:09:0a", False,
-                             10, 3, 5, "10.10.10.1", "11.11.11.1", 0, 64, 115,
-                             0, [], 8888, 9090, 100, 101, 5, 0, 2, 0, 0, False,
-                             100)
-p.to_vec()
+# use the Packet helper function to create ethernet header
+eth = Packet.ethernet("00:11:11:11:11:11", "00:11:11:11:11:11", 0x8100)
+eth.show()
 
-p = Packet.create_ipv4_packet("00:11:11:11:11:11", "00:06:07:08:09:0a", False,
-                              10, 3, 5, "10.10.10.1", "11.11.11.1", 6, 0, 64,
-                              115, 0, [], 100)
-p.show()
-
-e = Packet.ethernet("00:11:11:11:11:11", "00:11:11:11:11:11", 0x8100)
-e.show()
-
-p = p + e
-p.show()
-
-x = e + v
-x.show()
-
-pkt = Packet(200)
-e = Ethernet()
-e.etype = 0x8100
-v = Vlan()
-i = IPv4()
-u = UDP()
-pkt = pkt + e + v + i + u
+# create a new packet of size 100
+pkt = Packet(100)
+# append above ethernet header
+pkt = pkt + Ethernet()
 pkt.show()
 
+# headers can also be appended to obtained a packet
+pkt = Ethernet() + Vxlan()
+pkt.show()
+
+# create a TCP packet appending each header
 pkt = Packet(100) + Ethernet() + IPv4() + TCP()
 pkt.show()
+
+# create a TCP packet using the Packet helper function
+pkt = Packet.create_tcp_packet("00:11:11:11:11:11", "00:06:07:08:09:0a", False,
+                               10, 3, 5, "10.10.10.1", "11.11.11.1", 0, 64,
+                               115, 0, [], 8888, 9090, 100, 101, 5, 0, 2, 0, 0,
+                               False, 100)
+# convert packet to byte array
+pkt.to_vec()
+
+# duplicate a packet using clone method
+new_pkt = pkt.clone()
--- examples/pkt.rs
+++ examples/pkt.rs
@@ -1,8 +1,59 @@
 extern crate rscapy;
 
-use rscapy::headers::Ethernet;
+use rscapy::headers::*;
+use rscapy::Packet;
 
 fn main() {
-    let e = Ethernet::new();
-    e.show()
+    // create a simple Ethernet header
+    let mut eth = Ethernet::new();
+    eth.show();
+
+    // get the etype field from the header
+    let etype = eth.etype();
+    println!("{}", etype);
+
+    // update the etype field in the header
+    eth.set_etype(0x8100);
+    eth.show();
+
+    // use the Packet associate function to create ethernet header
+    let eth = Packet::ethernet("00:11:11:11:11:11", "00:11:11:11:11:11", 0x8100);
+    eth.show();
+
+    // create a UDP packet by pushing each header in sequence
+    let mut pkt = Packet::new(100);
+    pkt.push(Ethernet::new());
+    pkt.push(IPv4::new());
+    pkt.push(UDP::new());
+    pkt.show();
+
+    // convert packet to byte array
+    let v = pkt.to_vec();
+    println!("{:?}", v);
+
+    // duplicate a packet using clone
+    let new_pkt = pkt.clone();
+    new_pkt.show();
+
+    // create a TCP packet using the Packet associate function
+    let pkt = Packet::create_udp_packet(
+        "00:01:02:03:04:05",
+        "00:06:07:08:09:0a",
+        false,
+        10,
+        3,
+        5,
+        "192.168.0.199",
+        "192.168.0.1",
+        0,
+        64,
+        0,
+        0x4000,
+        Vec::new(),
+        1234,
+        9090,
+        false,
+        129,
+    );
+    pkt.show();
 }
